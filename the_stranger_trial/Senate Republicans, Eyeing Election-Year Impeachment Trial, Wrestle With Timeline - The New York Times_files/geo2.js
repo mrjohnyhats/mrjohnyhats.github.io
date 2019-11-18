a73bf8ce5a4f5fbd5b165d8305a452628a22f01d
Jonathan Rosen <jonbutterminecraftian@gmail.com>
2019-11-18 12:07:26 -0800

    added stranger

---
+++ the_stranger_trial/Senate Republicans, Eyeing Election-Year Impeachment Trial, Wrestle With Timeline - The New York Times_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "REDDING",
+      'continent': "NA",
+      'country': "US",
+      'region': "CA"
+    },
+    'ip':"67.218.106.98"
+  }]);
+})
+//
+()
+
+;
