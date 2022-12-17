6bd62b94f4fdeb9a6f525d7d0fd183bf2267c746
Jin Reezen <jinreezen@gmail.com>
2022-12-16 21:05:14 -0800

    new website

---
+++ js.js
@@ -0,0 +1,14 @@
+$(document).ready(()=>{
+	$("#menu").hide();
+	$("#content").hide();
+
+	$("#menu > #header").fadeIn(500);
+
+	$("#menu").fadeIn(1500);
+
+	$("#menu > button").fadeIn(3000);
+
+	$("#content").fadeIn(4500);
+});
+
+
